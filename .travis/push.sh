84baba39325c71a73e009279c4767e98980b58ae
Tapan Chandra <realalgomaniac@gmail.com>
2017-07-16 10:12:20 +0530

    travis config fixes

--- .travis/push.sh
+++ .travis/push.sh
@@ -9,6 +9,7 @@ setup_git() {
 upload_files() {  
   git add . -A
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  echo "https://${GITHUB_TOKEN}@github.com/algomaniac/awesome-scoop"
   git push --quiet "https://${GITHUB_TOKEN}@github.com/algomaniac/awesome-scoop" master:master > /dev/null 2>&1
 }
 
